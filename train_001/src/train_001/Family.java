050b08237bc7d5075a647814ba38ed59d1181506
owenzh <zhangxudong_sun@sina.com>
2018-07-31 06:45:28 +0800

    Create Family Class

---
+++ train_001/src/train_001/Family.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package train_001;
+
+/**
+ * @author zyyzxd
+ *
+ */
+public class Family {
+
+	/**
+	 * 
+	 */
+	public Family() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
