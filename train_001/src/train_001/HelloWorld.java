384d56d79e7c07267eb5d048c7b25ee21e67088f
owenzh <zhangxudong_sun@sina.com>
2018-07-31 12:58:28 +0800

    Primitive type test

--- train_001/src/train_001/HelloWorld.java
+++ train_001/src/train_001/HelloWorld.java
@@ -2,7 +2,7 @@ package train_001;
 
 public class HelloWorld {
 	/*
-	 * 第一个Java程序 它将打印字符串 Hello World
+	 * 
 	 */
 	public static void main(String[] args) {
 		System.out.println("Hello World"); // 打印 Hello World
---
+++ train_001/src/train_001/PrimitiveTypeTest.java
@@ -0,0 +1,51 @@
+package train_001;
+
+public class PrimitiveTypeTest {
+
+	public static void main(String[] args) {
+		System.out.println("Primitive type test...");
+		byte b = 127;
+		short s = 32767;
+		int it = 1234;
+		long lg = -23245533;
+		float ft = 0.0f;
+		float flt = 123.67f;
+		double db = 0.0d;
+		double dlb = 89.34;
+		boolean bn = true;
+		char cr = 'C';
+		StringBuilder stringBuilder = new StringBuilder();
+		stringBuilder.append("byte= ");
+		stringBuilder.append(b);
+		stringBuilder.append("\n");
+		stringBuilder.append("short= ");
+		stringBuilder.append(s);
+		stringBuilder.append("\n");
+		stringBuilder.append("int= ");
+		stringBuilder.append(it);
+		stringBuilder.append("\n");
+		stringBuilder.append("long= ");
+		stringBuilder.append(lg);
+		stringBuilder.append("\n");
+		stringBuilder.append("float= ");
+		stringBuilder.append(ft);
+		stringBuilder.append("\n");
+		stringBuilder.append("float= ");
+		stringBuilder.append(flt);
+		stringBuilder.append("\n");
+		stringBuilder.append("double= ");
+		stringBuilder.append(db);
+		stringBuilder.append("\n");
+		stringBuilder.append("double= ");
+		stringBuilder.append(dlb);
+		stringBuilder.append("\n");
+		stringBuilder.append("boolean= ");
+		stringBuilder.append(bn);
+		stringBuilder.append("\n");
+		stringBuilder.append("char= ");
+		stringBuilder.append(cr);
+		stringBuilder.append("\n");
+		System.out.println(stringBuilder.toString());
+
+	}
+}
